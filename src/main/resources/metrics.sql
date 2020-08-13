756d7b3f045cbe5febe3a3b5e7d0e8f0eade8d28
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-08-13 12:59:31 -0500

    SQL for metrics

---
+++ src/main/resources/metrics.sql
@@ -0,0 +1,13 @@
+ select json_arrayagg(result.json) as json from (SELECT json_object(
+               'date', date,
+               'avg_response_time', avg(time),
+               'total_request', (select count(request_in) as count from metrics ci where request_in=1 and ci.date = m.date group by request_in),
+               'avg_response_time_api_calls', (select avg(time) as count from metrics co where request_in=0 and co.date = m.date group by request_in),
+               'info_request', (select json_array(json_arrayagg(json_object('status_code', ir.code, 'count', ir.count))) as count
+                                from (select code, count(code) as count
+                                      from metrics cr where cr.date = m.date and cr.code is not null
+                                      group by date, code) as ir)
+           ) as json
+FROM metrics m group by date) as result
+
+
